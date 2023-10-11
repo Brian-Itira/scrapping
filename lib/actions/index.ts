c6a51a3bf95e3b30fc6da9e9ccd0a47bcfeee1d9
Brian-Itira <136370929+Brian-Itira@users.noreply.github.com>
2023-10-11 20:15:03 +0000

    Update index.ts

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,14 +1,16 @@
-import { scrapeJumiaProduct } from "../scraper";
+import { scrapeJumiaProduct, Product } from "../scraper"; // Assuming Product is the correct type
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
-    const scrapedProduct: any = await scrapeJumiaProduct(productUrl);
+    const scrapedProduct: Product | undefined = await scrapeJumiaProduct(productUrl);
 
     if (!scrapedProduct) return;
 
-    let product: any = scrapedProduct;
+    const product: Product = scrapedProduct;
+
+    // Now you can use 'product' with type safety.
   } catch (error: any) {
     throw new Error(`Failed to get product: ${error.message}`);
   }
