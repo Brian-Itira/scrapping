938ac77a886f45c33107dbfb96904f988bda9be3
Brian-Itira <136370929+Brian-Itira@users.noreply.github.com>
2023-10-11 20:07:46 +0000

    Update index.ts

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -4,11 +4,11 @@ export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
-    const scrapedProduct = await scrapeJumiaProduct(productUrl);
+    const scrapedProduct: any = await scrapeJumiaProduct(productUrl);
 
     if (!scrapedProduct) return;
 
-    let product = scrapedProduct;
+    let product: any = scrapedProduct;
   } catch (error: any) {
     throw new Error(`Failed to get product: ${error.message}`);
   }
